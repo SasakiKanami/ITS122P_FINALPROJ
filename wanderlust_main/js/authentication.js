2144ffc3e48ad9b0666a8c6ead89d19967aead5f
SasakiKanami <kianaaronrivera@gmail.com>
2026-06-16 09:36:55 +0800

    cleared conflicts
    cannot merge without clearing these

--- wanderlust_main/js/authentication.js
+++ wanderlust_main/js/authentication.js
@@ -30,19 +30,14 @@ import {
                 await setDoc(doc(db, "users", user.uid), {
                     username: username,
                     email: email,
-<<<<<<< HEAD
                     createdAt: new Date(),
                     password: password
-=======
-                    createdAt: new Date()
->>>>>>> feature/user-pages-gavin
                 });
 
                 alert("Registration successful! Please login.");
                 window.location.href = "login.html";
 
             } catch (error) {
-<<<<<<< HEAD
                 switch (error.code) {
                     case 'auth/email-already-in-use':
                         alert("This email is already registered! Please login instead.");
@@ -56,9 +51,6 @@ import {
                     default:
                         alert("Error: " + error.message);
                 }
-=======
-                alert("Error: " + error.message);
->>>>>>> feature/user-pages-gavin
             }
         });
     }
@@ -70,11 +62,7 @@ import {
         loginForm.addEventListener("submit", async (e) => {
             e.preventDefault();
 
-<<<<<<< HEAD
-            const email = loginForm.querySelector("input[placeholder='Email']").value.trim();
-=======
             const email = loginForm.querySelector("input[placeholder='Email']").value.trim(); //this should not be placeholder='Username' because we are using email to login, not username
->>>>>>> feature/user-pages-gavin
             const password = loginForm.querySelector("input[placeholder='Password']").value;
 
             try {
@@ -99,11 +87,7 @@ import {
 
             try {
                 await sendPasswordResetEmail(auth, email);
-<<<<<<< HEAD
                 alert("Password reset email sent! Check your inbox or Spam folder.");
-=======
-                alert("Password reset email sent! Check your inbox.");
->>>>>>> feature/user-pages-gavin
             } catch (error) {
                 alert("Error: " + error.message);
             }
@@ -118,10 +102,6 @@ import {
             await signOut(auth);
             window.location.href = "login.html";
         });
-<<<<<<< HEAD
     }
 
     // 
-=======
-    }
->>>>>>> feature/user-pages-gavin
