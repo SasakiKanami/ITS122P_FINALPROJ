92786149bc8f40be7bd9e3cb380b5613d42e4468
ERR0R404-notfound <marvinkarlrsangco040806@outlook.com>
2026-06-17 20:47:46 +0800

    Fixed the megre changes

--- wanderlust_main/js/authentication.js
+++ wanderlust_main/js/authentication.js
@@ -30,35 +30,14 @@ import {
                 await setDoc(doc(db, "users", user.uid), {
                     username: username,
                     email: email,
-<<<<<<< HEAD
-                    createdAt: new Date(),
-                    password: password
-=======
                     createdAt: new Date()
->>>>>>> feature/user-pages-gavin
                 });
 
                 alert("Registration successful! Please login.");
                 window.location.href = "login.html";
 
             } catch (error) {
-<<<<<<< HEAD
-                switch (error.code) {
-                    case 'auth/email-already-in-use':
-                        alert("This email is already registered! Please login instead.");
-                        break;
-                    case 'auth/invalid-email':
-                        alert("Please enter a valid email address.");
-                        break;
-                    case 'auth/weak-password':
-                        alert("Password must be at least 6 characters.");
-                        break;
-                    default:
-                        alert("Error: " + error.message);
-                }
-=======
                 alert("Error: " + error.message);
->>>>>>> feature/user-pages-gavin
             }
         });
     }
@@ -70,13 +49,8 @@ import {
         loginForm.addEventListener("submit", async (e) => {
             e.preventDefault();
 
-<<<<<<< HEAD
             const email = loginForm.querySelector("input[placeholder='Email']").value.trim();
-=======
-            const email = loginForm.querySelector("input[placeholder='Email']").value.trim(); //this should not be placeholder='Username' because we are using email to login, not username
->>>>>>> feature/user-pages-gavin
             const password = loginForm.querySelector("input[placeholder='Password']").value;
-
             try {
                 await signInWithEmailAndPassword(auth, email, password);
                 alert("Login successful! Welcome back!");
@@ -99,11 +73,7 @@ import {
 
             try {
                 await sendPasswordResetEmail(auth, email);
-<<<<<<< HEAD
-                alert("Password reset email sent! Check your inbox or Spam folder.");
-=======
                 alert("Password reset email sent! Check your inbox.");
->>>>>>> feature/user-pages-gavin
             } catch (error) {
                 alert("Error: " + error.message);
             }
@@ -118,10 +88,4 @@ import {
             await signOut(auth);
             window.location.href = "login.html";
         });
-<<<<<<< HEAD
     }
-
-    // 
-=======
-    }
->>>>>>> feature/user-pages-gavin
